907619f48343fb1e9e4a1647b272dfc20b2a6228
1badr <badr00hussin@gmail.com>
2024-02-18 20:46:59 +0300

    v5

--- models/CV.js
+++ models/CV.js
@@ -14,11 +14,11 @@ const CVSchema = new mongoose.Schema ({
     email : {
         type : String,
     },
-    languages: [{type: Array }],
-    education: [{type: Array }],
-    experience: [{type: Array}],
-    skills: [{type: Array }],
-    certificate: [{type: Array }],
+    languages: {type: Array },
+    education: {type: Array },
+    experience: {type: Array},
+    skills: {type: Array },
+    certificate: {type: Array },
 });
 
 
