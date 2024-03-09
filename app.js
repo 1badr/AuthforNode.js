b02866294b0c93ebf24fd7c528059fb58bd121a6
1badr <badr00hussin@gmail.com>
2024-03-09 17:55:36 +0300

    v6

--- app.js
+++ app.js
@@ -73,10 +73,13 @@ app.use('/quiz',quizRoute);
 
 
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,PATCH,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-app.use(cors({
-  origin: "http://localhost:3000",
-}));
 
 
 ///========================================================
