9d5c452691a15a68463123b25ac7b3718473f448
1badr <badr00hussin@gmail.com>
2024-05-21 17:49:58 +0300

    v33

--- models/Save.js
+++ models/Save.js
@@ -1,11 +1,11 @@
 const mongoose = require ('mongoose');
 
-const SaveSchema = new mongoose.Schema ({
-    IDUser: [{type: String, ref: "User", required: true}],
-    IDblog: [{type: String, ref: "Blogs", required: true}],
-    likes_count:  { type: Number, default: 0 }
-
+const SaveSchema = new mongoose.Schema({
+  IDUser: { type: String, ref: 'User' },
+  IDblog: { type: String, ref: 'Blogs' },
+  state: { type: Boolean, default: null },
 });
+
 const Save = mongoose.model('Save', SaveSchema);
 
 module.exports = Save;
