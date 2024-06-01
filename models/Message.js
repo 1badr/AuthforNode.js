f2b8ea9e8f4dda523ff6b5bb132305db3121aa5c
1badr <badr00hussin@gmail.com>
2024-06-01 11:12:43 +0300

    v37

--- models/Message.js
+++ models/Message.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
 
-const MessageSchema = mongoose.model('Messages', {
+const MessageSchema = new mongoose.Schema({
   conversationId: { type: String, ref: 'Conversation' },
   sender: { type: String, ref: 'User' },
   recipient: { type: String, ref: 'User' },
   text: String,
   createdAt: { type: Date, default: Date.now }
 });
+
 const Message = mongoose.model('Message', MessageSchema);
 
 module.exports = Message;
